5478443af3689711a2bedaf6a338cf07ce584011
VishankWagh <vishankswagh@gmail.com>
2025-09-08 18:00:27 +0530

    added cors with new url

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(
     cors({
-        origin: "http://localhost:5173"
+        origin: "https://ecommece-vsw.vercel.app"
     })
 );
 app.use(express.json());
